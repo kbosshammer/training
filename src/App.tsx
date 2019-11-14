673338732b939cfeeafb2ecad540f35b69e0e3c5
wiju1013wj <33145568+wiju1013wj@users.noreply.github.com>
2019-11-14 17:19:08 +0100

    AnlegenAnfang (#4)
    * AnlegenAnfang
    
    Anlegen Ordner mit Gruppenname, samt Einstiegskomponente
    
    * Fhi einfügen
    
    wieder eingefügt

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { Box, Divider, Typography } from '@material-ui/core'
 import React from 'react'
 
 import Fhi from './fhi/Fhi'
+import Wiju1013wj from './wiju1013wj/Wiju1013wj'
 import Ts from './teamsalbei/Ts'
 
 const App = () => (
     <Box margin={4}>
         <Typography variant="h6" gutterBottom align="center">
-            heute ist der {new Date().toLocaleDateString()} - viel Spaß
+            Heute ist der {new Date().toLocaleDateString()} - Viel Spaß
         </Typography>
         <Divider />
         {/* ab hier bitte eure "Einstiegskomponente" einfügen, bspw.: */}
         <Fhi />
+        <Wiju1013wj />
         <Ts />
     </Box>
 )
---
+++ src/wiju1013wj/Wiju1013wj.tsx
@@ -0,0 +1,6 @@
+import { TextField } from '@material-ui/core'
+import React from 'react'
+
+const wiju1013wj = () => <TextField margin="dense" fullWidth label="wiju1013wj" />
+
+export default wiju1013wj
